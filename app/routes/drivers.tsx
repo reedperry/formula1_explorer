2ceb0057a9708bb41d75fabfdbc2160efc489a3a
Reed Perry <reedperry@users.noreply.github.com>
2023-02-15 20:48:11 -0700

    Update healthcheck and drivers route to start reading new F1 data

--- app/routes/drivers.tsx
+++ app/routes/drivers.tsx
@@ -1,9 +1,11 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
 export async function loader({ request }: LoaderArgs) {
-  return json({});
+  const drivers = await prisma.driver.findMany({ take: 10 });
+  return json({ drivers });
 }
 
 export default function DriversPage() {
@@ -18,6 +20,13 @@ export default function DriversPage() {
       </header>
 
       <main className="flex h-full bg-white">
+        <ul>
+          {data.drivers.map(driver => {
+            return <li key={driver.code}>
+              <span>{driver.number}: {driver.forename} {driver.surname}</span>
+            </li>
+          })}
+        </ul>
         <div className="flex-1 p-6">
           <Outlet />
         </div>
