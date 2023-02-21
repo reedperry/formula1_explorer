214cd33f5d6f2bbf3c949c55bb60f462abd1d2ef
Reed Perry <reedperry@users.noreply.github.com>
2023-02-20 17:34:12 -0700

    Get basic data load and display going for a circuits list

---
+++ app/routes/circuits.tsx
@@ -0,0 +1,77 @@
+
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
+
+export async function loader({ request }: LoaderArgs) {
+  const circuits = await prisma.circuit.findMany({
+    select: {
+      circuitId: true,
+      circuitRef: true,
+      alt: true,
+      country: true,
+      location: true,
+      name: true,
+      url: true,
+      races: {
+        where: {
+          year: 2022
+        },
+        orderBy: {
+          date: 'desc'
+        },
+        take: 1,
+        select: {
+          date: true,
+          results: {
+            where: {
+              positionText: "1"
+            },
+            include: {
+              drivers: true
+            }
+          }
+        }
+
+      }
+    },
+    orderBy: {
+      name: 'asc'
+    },
+  });
+
+  return json({ circuits });
+}
+
+export default function CircuitsPage() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">
+          <Link to=".">Circuits</Link>
+        </h1>
+      </header>
+
+      <main className="flex h-full bg-white">
+        <ul>
+          {data.circuits.map(circuit => {
+            return <li key={circuit.circuitRef}>
+              <Link to={circuit.circuitId.toString()}>
+                <span>{circuit.name} ({circuit.country})</span>
+              </Link>
+              <div>{circuit.races.map(r =>
+                <small>Winner on {r.date} - {r.results[0].drivers.surname}</small>
+              )}</div>
+            </li>
+          })}
+        </ul>
+        <div className="flex-1 p-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
